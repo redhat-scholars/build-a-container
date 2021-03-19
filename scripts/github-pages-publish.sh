068c312de3716ddac1a98d5182cb6a5fa3b6bbc3
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2021-03-19 03:27:37 +0000

    Fixes to publishing site to Github pages

--- scripts/github-pages-publish.sh
+++ scripts/github-pages-publish.sh
@@ -3,22 +3,27 @@
 set -euo pipefail
 
 declare SITE=${1:-site.yml}
-declare REPO=${2:-https://github.com/hatmarch/cdc-data-monolith}
+declare REPO=${2:-https://github.com/hatmarch/container-workshop}
 declare BRANCH="gh-pages"
 
 echo "Removing old publish directory"
-if [[ -d $DEMO_HOME/publish ]]; then
-    rm -rf $DEMO_HOME/publish 
+if [[ -d $DEMO_HOME/gh-publish ]]; then
+    rm -rf $DEMO_HOME/gh-publish 
 fi
 
-git clone -b ${BRANCH} ${REPO} $DEMO_HOME/publish
+echo "Removing antora cache directory"
+if [[ -d $DEMO_HOME/.cache ]]; then
+    rm -rf $DEMO_HOME/.cache 
+fi
+
+git clone -b ${BRANCH} ${REPO} $DEMO_HOME/gh-publish
 
 echo "Generating the site documentation from ${SITE}"
 
-antora generate --stacktrace $DEMO_HOME/${SITE} --to-dir $DEMO_HOME/publish
+antora generate --stacktrace $DEMO_HOME/${SITE} --to-dir $DEMO_HOME/gh-publish
 
 echo "Pushing site to ${BRANCH} branch of ${REPO}"
-cd $DEMO_HOME/publish
+cd $DEMO_HOME/gh-publish
 git add --all .
 git commit -m"Automated Publish" 
 git push origin
