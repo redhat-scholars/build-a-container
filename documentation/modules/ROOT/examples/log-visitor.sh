85cd74c5223a8baef8aa5ddc23e9b5b2b4ae9817
Marc Hildenbrand <mwhildenbrand+github@gmail.com>
2021-07-14 05:15:28 +0000

    Updates to security section
    Move container security back into main section
    
    Make metasploit and normal terminal more clear
    
    Address some missing images and typos
    
    Remove SELinux and Privileged container sections (and link to SECURITY module for now)
    
    Update SECURITY module version of the container security section

---
+++ documentation/modules/ROOT/examples/log-visitor.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+VISITOR=$(echo $QUERY_STRING | sed -rn 's/visitor=([^&]+)&?.*/\1/p')
+echo $VISITOR >> /var/log/www/visitor_info.txt
+
+cat <<EOF
+Content-type: text/html
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+<body>
+<title>Thank You</title>
+<h1>Thank you pass:[${VISITOR}]!</h1>
+I have collected your info as user: $(whoami) <br> <br>
+
+Click <a href=/hello.html>here</a> to register another visitor.
+</body>
+</html>
+EOF
