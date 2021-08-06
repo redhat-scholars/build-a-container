69c18ecc15de1f1bfeac1713accc0924557d6fa2
Marc Hildenbrand <mwhildenbrand@gmail.com>
2021-08-06 08:24:29 +0000

    Improvements to publish

--- scripts/github-pages-publish.sh
+++ scripts/github-pages-publish.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 declare SITE=${1:-site.yml}
 declare REPO=${2:-$(git remote get-url origin)}
 declare BRANCH="gh-pages"
+declare DATE_TIME=`date`
 
 echo "Removing old publish directory"
 if [[ -d $DEMO_HOME/gh-publish ]]; then
@@ -16,7 +17,13 @@ if [[ -d $DEMO_HOME/.cache ]]; then
     rm -rf $DEMO_HOME/.cache 
 fi
 
+echo "Cloning repo"
 git clone -b ${BRANCH} ${REPO} $DEMO_HOME/gh-publish
+
+echo "Pulling images"
+cd $DEMO_HOME/gh-publish/
+git pull
+cd $DEMO_HOME
 
 echo "Generating the site documentation from ${SITE}"
 
@@ -25,7 +32,6 @@ antora generate --stacktrace $DEMO_HOME/${SITE} --to-dir $DEMO_HOME/gh-publish
 echo "Pushing site to ${BRANCH} branch of ${REPO}"
 cd $DEMO_HOME/gh-publish
 git add --all .
-git commit -m"Automated Publish" 
-git push origin
-
+git commit -m "Automated Publish ${DATE_TIME}" 
+git push origin ${BRANCH}
 echo "Site published successfully!"
